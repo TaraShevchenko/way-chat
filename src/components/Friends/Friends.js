70be8ef746069348594c1fcee08f7df7d221285e
unknown <tarasbizzz@gmail.com>
2021-07-01 17:46:12 +0300

    delete superfluous
    delete superfluous

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -4,7 +4,6 @@ import x from "./Friends.module.scss";
 
 import Search from "../Chat/Search/Search";
 import FriendsList from "./FriendsList/FriendsList";
-import PrimaryButton from "../Indifferent/PrimaryButton/PrimaryButton";
 import FriendsGlobalSearch from "./FriendsGlobalSearch/FriendsGlobalSearch";
 
 import {Route} from "react-router-dom";
@@ -14,17 +13,16 @@ const Friends = () => {
     return (
         <>
             <div className="content-small position-relative">
-
                 <div className="d-flex justify-content-between align-items-center mb-4">
                     <div className={`${chat.title}`}>Friends</div>
-                    <PrimaryButton url='/Friends/GlobalSearch'>
+                    <button className="primaryButton">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                              className="bi bi-person-plus-fill" viewBox="0 0 16 16">
                             <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                             <path fill-rule="evenodd"
                                   d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                         </svg>
-                    </PrimaryButton>
+                    </button>
                 </div>
                 <Search/>
                 <div className={chat.list}>
@@ -42,10 +40,9 @@ const Friends = () => {
                         TestAlien
                     </div>
                 </Route>
-
             </div>
-            <div className="content-big position-relative">
 
+            <div className="content-big position-relative">
                 <FriendsGlobalSearch/>
 
                 <Route path='/Friends/my/1'>
@@ -59,7 +56,6 @@ const Friends = () => {
                         TestAlien
                     </div>
                 </Route>
-
             </div>
         </>
     );
--- src/components/Friends/FriendsGlobalSearch/FriendsGlobalSearch.js
+++ src/components/Friends/FriendsGlobalSearch/FriendsGlobalSearch.js
@@ -2,11 +2,10 @@ import React from 'react';
 import x from "./FriendsGlobalSearch.module.scss";
 import Search from "../../Chat/Search/Search";
 import Ava from "../../Indifferent/Ava/Ava";
-import PrimaryButton from "../../Indifferent/PrimaryButton/PrimaryButton";
 
 const FriendsGlobalSearch = () => {
     return (
-        <div className={x.globalSearch}>
+        <div>
             <div className={x.searchWrapper}>
                 <Search placeholder='Search friends..'/>
             </div>
@@ -33,13 +32,13 @@ const FriendsGlobalSearch = () => {
                     </div>
                 </div>
 
-                <PrimaryButton>
+                <button className="primaryButton">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-plus-lg" viewBox="0 0 16 16">
                         <path
                             d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
                     </svg>
-                </PrimaryButton>
+                </button>
 
             </div>
         </div>
